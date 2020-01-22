b0a34976cadf49e791c71738b6800c359a64541d
emma12812 <emma12812@gmail.com>
2020-01-22 09:58:28 +0000

    Session two extension complete

--- AAA_MyClassWork/SessonTwoSecondActivity/app/src/main/java/com/example/reportproblems/MainActivity.java
+++ AAA_MyClassWork/SessonTwoSecondActivity/app/src/main/java/com/example/reportproblems/MainActivity.java
@@ -5,13 +5,17 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.view.View.OnClickListener;
 import android.view.View;
-import android.widget.TextView;
 import android.widget.EditText;
+
+import java.util.concurrent.atomic.AtomicInteger;
 
 
 public class MainActivity extends AppCompatActivity implements OnClickListener {
 
+
     /** Called when the activity is first created. */
+    private static AtomicInteger numberClicks = new AtomicInteger(0);
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,17 +29,25 @@ public class MainActivity extends AppCompatActivity implements OnClickListener {
     @Override
     public void onClick(View view) {
 
+        EditText et = (EditText)findViewById(R.id.et1);
+        et.setText("");
         switch (view.getId()) {
-
             case R.id.btn1:
-                // do your code
+                // clear
+                et.setHint("@string/");
                 break;
 
             case R.id.btn2:
-                // do your code
+                // ok
+                String msg1 = getString(R.string.acknowlege);
+                int i = numberClicks.addAndGet(1);
+                msg1 = String.format(msg1, Integer.toString(i)); // Thank you for reporting your problem. Problem id:%s
+                et.setText("");
+                et.setHint(msg1);
                 break;
 
             default:
+
                 break;
         }
 
